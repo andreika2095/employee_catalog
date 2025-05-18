8f8d61b5cc7fd789f6274715f282d3a931c069e2
andreika2095 <andreika2095@yandex.ru>
2025-05-18 17:51:20 +0300

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost/employee_catalog"
-
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-session = Session()
